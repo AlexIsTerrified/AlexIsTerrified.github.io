db93706d814376887d4aea5471e7b7c4e6fc8013
unknown <alexepicity@gmail.com>
2019-10-10 17:01:04 -0400

    Initial commit

--- profile.js
+++ profile.js
@@ -3,3 +3,16 @@ function autoScroll(posID){
 		scrollTop: $('#'+posID).offset().top
 	},1000)
 }
+var menu = document.querySelector('.menu');
+
+function menuCl(){
+	menuEl = $(".menu");
+	nav = $("#profile nav");
+	if(menuEl.hasClass('close')){
+		menuEl.removeClass('close');
+		nav.removeClass('menu-display');
+	}else{
+		menuEl.addClass('close');
+		nav.addClass('menu-display');
+	}
+}
